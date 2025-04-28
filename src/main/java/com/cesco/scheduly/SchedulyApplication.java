405e84b7b428b05b1dbae6ce576df346a521cb3f
Yejin <dpwls8972@gmail.com>
2025-04-28 15:57:34 +0900

    commit

---
+++ src/main/java/com/cesco/scheduly/SchedulyApplication.java
@@ -0,0 +1,13 @@
+package com.cesco.scheduly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cesco/scheduly/SchedulyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesco.scheduly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
