355362638b06b6e93e7f179c53f202c857f80756
Jayu-Lee <114810429+Jayu-Lee@users.noreply.github.com>
2025-05-12 01:01:23 +0900

    Create SignupRequest.java
    회원가입 요청

---
+++ src/main/java/com/cesco/scheduly/dto/SignupRequest.java
@@ -0,0 +1,10 @@
+@Getter @Setter
+public class SignupRequest {
+    private String student_id;
+    private String password;
+    private String name;
+    private String major;
+    private String double_major;
+    private int grade;
+    private int semester;
+    }
