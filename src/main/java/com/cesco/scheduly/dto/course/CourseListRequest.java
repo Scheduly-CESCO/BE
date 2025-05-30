b0f2d70708b5a449a3decc94e71398fc8a848ec8
Yejin Kim <86982496+dpwls8984@users.noreply.github.com>
2025-05-31 02:51:47 +0900

    Delete CourseListRequest

--- src/main/java/com/cesco/scheduly/dto/course/CourseListRequest.java
+++
@@ -1,9 +0,0 @@
-package com.cesco.scheduly.dto.course;
-
-import lombok.Data;
-import java.util.List;
-
-@Data
-public class CourseListRequest {
-    private List<String> courseCodes; // 학수번호 리스트
-}
