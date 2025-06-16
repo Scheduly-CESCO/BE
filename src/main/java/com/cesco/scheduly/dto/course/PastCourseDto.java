a0a93d4c4b1269f88145506ee396f1d2c5359620
Yejin <dpwls8972@gmail.com>
2025-06-16 15:51:37 +0900

    fix: 변수명

--- src/main/java/com/cesco/scheduly/dto/course/PastCourseDto.java
+++ src/main/java/com/cesco/scheduly/dto/course/PastCourseDto.java
@@ -6,15 +6,19 @@ import lombok.Data;
 @Data
 public class PastCourseDto {
 
-    @JsonProperty("courseCode")
+    // JSON의 "학수번호" 키를 courseCode 필드에 매핑
+    @JsonProperty("학수번호")
     private String courseCode;
 
-    @JsonProperty("department")
+    // JSON의 "개설영역" 키를 department 필드에 매핑
+    @JsonProperty("개설영역")
     private String department;
 
-    @JsonProperty("courseName")
+    // JSON의 "교과목명" 키를 courseName 필드에 매핑
+    @JsonProperty("교과목명")
     private String courseName;
 
-    @JsonProperty("professor")
+    // JSON의 "담당교수" 키를 professor 필드에 매핑
+    @JsonProperty("담당교수")
     private String professor;
 }
